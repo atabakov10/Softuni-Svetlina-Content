1329293f571e72462ed624ef0fdf7f2a6ca82ec9
ANGEL <atabakov99@abv.bg>
2022-12-29 13:19:04 +0200

    Updates on code

--- Programming-Fundamentals-Strings-Maps/Wordle/WordleForm.Designer.cs
+++ Programming-Fundamentals-Strings-Maps/Wordle/WordleForm.Designer.cs
@@ -485,6 +485,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
 			this.ClientSize = new System.Drawing.Size(419, 309);
 			this.Controls.Add(this.btnHint);
 			this.Controls.Add(this.btnReset);
